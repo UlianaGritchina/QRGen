976e03eb48d7c2e06e9bff8973dfaa4bc69af381
Uliana Gritchina <ugritchina@icloud.com>
2022-09-26 18:48:15 +0300

    Added QRCodeView

---
+++ Sources/QRGen/QRCodeView/QRCodeView.swift
@@ -0,0 +1,50 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Ульяна Гритчина on 26.09.2022.
+//
+
+
+import SwiftUI
+@available(iOS 14.0, *)
+@available(macOS 10.15, *)
+
+struct QRCodeView: View {
+    @ObservedObject var vm: QRCodeViewModel
+    init(string: String,
+         foregroundColor: Color? = .black,
+         backgroundColor: Color? = .white,
+         width: CGFloat? = 200,
+         height: CGFloat? = 200,
+         cornerRadius: CGFloat? = 0) {
+        vm = QRCodeViewModel(string: string,
+                             foregroundColor: foregroundColor,
+                             backgroundColor: backgroundColor,
+                             width: width,
+                             height: height,
+                             cornerRadius: cornerRadius)
+    }
+    var body: some View {
+        VStack {
+            if let image = UIImage(data: vm.qrImageData) {
+                Image(uiImage: image)
+                    .resizable()
+                    .frame(width: vm.width, height: vm.height)
+                    .cornerRadius(vm.radius)
+            }
+            
+        }
+        .onAppear { vm.generateQRCode() }
+    }
+    
+}
+
+
+@available(iOS 14.0, *)
+@available(macOS 10.15, *)
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        QRCodeView(string: "fff", backgroundColor: .blue, cornerRadius: 20)
+    }
+}
--- Sources/QRGen/QRGen.swift
+++ Sources/QRGen/QRGen.swift
@@ -1,25 +1,8 @@
-import Foundation
-import CoreImage.CIFilterBuiltins
-import SwiftUI
 
 public struct QRGen {
     public private(set) var text = "Hello, World!"
     
     public init() {
-    }
-    
-    public func generateQR(string: String) -> CIImage? {
-        let data = string.data(using: String.Encoding.utf8)
-        guard let filter = CIFilter(name: "CIQRCodeGenerator") else { return nil }
-        filter.setValue(data, forKey: "inputMessage")
-        filter.setValue("H", forKey: "inputCorrectionLevel")
-        guard let outputImage = filter.outputImage else { return nil }
-        let transform = CGAffineTransform(scaleX: 20, y: 20)
-        let scaledCIImage = outputImage.transformed(by: transform)
-        return scaledCIImage
-    }
-    
-    public func generateCustomQR() {
         
     }
     
